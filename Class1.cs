2fbc35360ae2a12c0b7af07e7cd1b45ce3368c49
Freemort <freemortus@gmail.com>
2020-05-26 10:04:07 +0300

    Starting Fresh

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Class1 : ChangeLevel
+{
+    public event EventHandler Event;
+
+    ~Class1()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Method()
+    {
+        throw new System.NotImplementedException();
+    }
+}
