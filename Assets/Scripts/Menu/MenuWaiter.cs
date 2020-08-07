e1d295d00e713b0140562638dd20fe22f7b613ef
Freemort <freemortus@gmail.com>
2020-08-07 11:00:44 +0300

    Scene fixes
    -Proper button names.

---
+++ Assets/Scripts/Menu/MenuWaiter.cs
@@ -0,0 +1,16 @@
+﻿
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class MenuWaiter : MenuBase
+{
+    protected override void Awaiters()
+    {
+        Thread.Sleep(5000);
+    }
+}
